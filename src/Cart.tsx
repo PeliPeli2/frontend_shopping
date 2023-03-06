4c31e201614fd5d1886e9b7abe27b45a97e4c498
Madkingz <79462699+Madkingz@users.noreply.github.com>
2023-03-06 19:30:12 +0100

    fixed initial total + initial amount of items

--- src/Cart.tsx
+++ src/Cart.tsx
@@ -2,30 +2,38 @@ import React, {useState } from "react"
 import CartData from './data.json'
 import * as CartItems from './CartItems'
 
-
 export function createCart() {
 
     var initialCount = 0
     
     var initialcost = 0
-    CartData.map((data: { price: any }) => {
+    CartData.map((data: { price: number }) => {
         initialcost = initialcost + data.price * initialCount
         
     })
-
+    
+    
     const [total, setTotal] = useState(initialcost)
 
+    if (total != 0){
     return (
         <div className = "cart">
         <div className = "cart-total" id = "cart-cost">
             {"Total Cart Cost: " + total + " DKK"}
         </div>
-        <div className = "item-row" >  
-        
-        <CartItems.cartItems CartData={CartData} setTotal={setTotal} total={total} />
-
-        
-
+        <div className = "item-row" >
+            <CartItems.cartItems CartData={CartData} setTotal={setTotal} total={total} />
+        </div>
+        </div>
+    )
+    }
+    else return (
+        <div className = "cart">
+        <div className = "empty-cart" id = "empty-cart">
+            {"Cart is empty"}
+        </div>
+        <div className = "item-row" >
+            <CartItems.cartItems CartData={CartData} setTotal={setTotal} total={total} />
         </div>
         </div>
     )
--- src/CartItems.tsx
+++ src/CartItems.tsx
@@ -1,63 +1,118 @@
-import React, {useState } from "react"
+import React, {useEffect, useState } from "react"
 
+interface cartItemsProps {CartData: ({
+    id: string;
+    name: string;
+    price: number;
+    currency: string;
+    rebateQuantity: number;
+    rebatePercent: number;
+    upsellProductId: null;
+} | {
+    id: string;
+    name: string;
+    price: number;
+    currency: string;
+    rebateQuantity: number;
+    rebatePercent: number;
+    upsellProductId: string;
+})[], 
+setTotal : (total : number) => void,
+total : number}
 
-export function cartItems({CartData, setTotal, total} : any) : any{
+export function cartItems({CartData, setTotal, total} : cartItemsProps){
+
+    var tmptotal = 0
+
     return (
-        CartData && CartData.map((productInfo: { name: string; price: number; id: string }) => {
+        <div>
+        {
+        
+        
 
-            const [count, setCount]  = useState(0);
-            const [show, setShow] = useState(Math.random());            
+        CartData && CartData.map((productInfo) => {
+
+
+
+
+            const [show, setShow] = useState(Math.random());        
+            const [count, setCount]  = useState(1);
             
-            function increment(cost : any) {
+            
+            function increment(cost : number) {
                 setTotal(total+cost)
                 setCount(count+1);    
             }
 
-            function decrement(cost : any) {
-                if (count != 0){
+            function incrementInitialTotal(price: number){
+                setTotal(total+price)
+            }
+            function decrement(cost : number) {
+                if (count != 1){
                 setCount(count-1);
                 setTotal(total-cost)    
                 }
             }
 
-            function deleteitem(id : any, cost : number, count : number) {
+            function deleteitem(id : string, cost : number, count : number) {
                 setTotal(total-(cost*count))
                 var product = document.getElementById("item"+id)
                 product?.remove()
+                
             }
 
-            if (show > 0.9){
+
+            // randomizer
+            if (show > 0.9){      
             
+                tmptotal = tmptotal+productInfo.price
+                useEffect(()=>{
+                    setTotal(tmptotal)
+                }, [tmptotal])
+            
+            
+
+
             return (
-                <div className = "item-card" id = {"item"+productInfo.id}>
+                <div key={"key"+productInfo.id} className = "item-card" id = {"item"+productInfo.id}>
                     <h2 className = "product-info" >
-                    {'Name: ' + productInfo.name + ' ' + 'Price: ' + productInfo.price + ' DKK' + ' pr. styk '}
+                    {'Name: ' + productInfo.name + ' ' + 'Price: ' + productInfo.price + " " + productInfo.currency + ' pr. styk '}
                     </h2>
                     
                     <div className = "item-amount">
                         {"Amount: " + count}
                     </div>
                     
-                    <div className = "adjusters">
+                    <div className = "adjusters" >
                         <button className = "decrement-button" onClick={() => decrement(productInfo.price,)} >
                         -
                         </button>
-                        <button className = "increment-button" onClick={() => increment(productInfo.price,)} >
+                        <button className = "increment-button"  onClick={() => increment(productInfo.price,)} >
                         +
                         </button>
+            
                     </div>
+                    
                     
                     <button className = "delete-button" onClick={() => deleteitem(productInfo.id, productInfo.price, count)} >
                     x
                     </button >
                     
                     <div className = "items-cost">
-                    {"Cost: " + count*productInfo.price + " DKK"} 
+                    {"Cost: " + count*productInfo.price + " " + productInfo.currency} 
                     </div>
+                    
                 
                 </div>
                 )
+                
             }
+            
         }
-        ))
+        )
+        }
+        </div>
+        )
+        
+        
 }
